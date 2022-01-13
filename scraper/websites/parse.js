36774e8de4f3062ef29bad20b820d63d3ccb6ac5
Marcus <marcus.gamboa@gmail.com>
2022-01-13 16:40:02 -0700

    Update file tree

--- scraper/websites/parse.js
+++
@@ -1,7 +0,0 @@
-export default str => {
-  const regex = /[0-9]*[0-9].[0-9]*/;
-  let match = str.match(regex)[0];
-
-  if (!parseFloat(match)) return "Error";
-  return parseFloat(match);
-};
--- scraper/websites/sofi.js
+++
@@ -1,5 +0,0 @@
-import puppeteer from "puppeteer";
-
-export default async () => {
-  console.log("https://www.sofi.com/money/");
-};
